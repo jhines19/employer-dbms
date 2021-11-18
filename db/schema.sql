4174df79be11777a8a1f165b07ea245e54ce2797
Jaraad Hines <jaraadhines@jaraads-mbp-2.myfiosgateway.com>
2021-11-18 03:12:28 -0500

    create database

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS management_tool;
+CREATE DATABASE management_tool;
+
+USE management_tool;
+
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments(
+    
+);
+
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles(
+
+);
+
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees(
+
+);
