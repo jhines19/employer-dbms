db74f112dcd439f06a83cd7723c182908273be46
Jaraad Hines <jaraadhines@jaraads-mbp-2.myfiosgateway.com>
2021-11-18 01:35:14 -0500

    add express middleware to server

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ const cTable = require('console.table');
 const PORT = process.env.PORT || 3001;
 const app = ('express');
 
+//Express middleware
 app.use(express.urlencoded({ extended : false }));
 app.use(express.json());
