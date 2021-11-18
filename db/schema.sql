cbab8afb35d53df0466e695245082d20d6ffa6e1
Jaraad Hines <jaraadhines@jaraads-mbp-2.myfiosgateway.com>
2021-11-18 03:25:55 -0500

    build out columns for roles table

--- db/schema.sql
+++ db/schema.sql
@@ -11,10 +11,16 @@ CREATE TABLE department(
 
 DROP TABLE IF EXISTS roles;
 CREATE TABLE roles(
-    
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    job_title VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    dep VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    FOREIGN KEY (dep) REFERENCES department(dep_name)
 );
 
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee(
-
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
 );
