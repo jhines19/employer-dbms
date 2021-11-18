2aa50c3d5d0b38bfc279d8b4535edc6d7eba285f
Jaraad Hines <jaraadhines@jaraads-mbp-2.myfiosgateway.com>
2021-11-18 01:03:06 -0500

    update

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express = require('express');
 const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = ('express');
+
+app.use(express.urlencoded({ extended : false }));
+app.use(express.json());
