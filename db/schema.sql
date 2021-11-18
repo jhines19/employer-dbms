e88a43c88555bba4e28ec6e11f5189a15809923e
Jaraad Hines <jaraadhines@jaraads-mbp-2.myfiosgateway.com>
2021-11-18 03:27:20 -0500

    build out employee table columns

--- db/schema.sql
+++ db/schema.sql
@@ -23,4 +23,9 @@ DROP TABLE IF EXISTS employee;
 CREATE TABLE employee(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+    ON DELETE SET NULL
 );
